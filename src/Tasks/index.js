43ecc5f0ba58c747173fb4b25b8a301152e432d7
Mariusz Matusiewicz <mariuszmmm@op.pl>
2022-12-05 20:31:24 +0100

    change key in tasks

--- src/Tasks/index.js
+++ src/Tasks/index.js
@@ -1,23 +1,24 @@
 import "./style.css";
 
 const Tasks = ({ tasks, hideDoneTasks }) => (
-    <ul className="tasks">
-        {tasks.map(task => (
-            <li key={tasks.id}
-                className={`tasks__item ${(task.done && hideDoneTasks) ? "task__item--hide" : ""}`}
-            >
-                <button className="tasks__button tasks__button--toggleDone">
-                    {task.done ? "✔" : ""}
-                </button>
-                <span className={`tasks__content ${task.done ? "tasks__content--toggleDone" : ""}`}>
-                    {task.content}
-                </span>
-                <button className="tasks__button tasks__button--remove">
-                    🗑︎
-                </button>
-            </li>
-        ))}
-    </ul >
+	<ul className="tasks">
+		{tasks.map(task => (
+			<li
+				key={task.id}
+				className={`tasks__item ${(task.done && hideDoneTasks) ? "task__item--hide" : ""}`}
+			>
+				<button className="tasks__button tasks__button--toggleDone">
+					{task.done ? "✔" : ""}
+				</button>
+				<span className={`tasks__content ${task.done ? "tasks__content--toggleDone" : ""}`}>
+					{task.content}
+				</span>
+				<button className="tasks__button tasks__button--remove">
+					🗑︎
+				</button>
+			</li>
+		))}
+	</ul >
 );
 
 export default Tasks;
