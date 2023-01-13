706430f49de049fedaeb66ac389fe2580965de2a
Mariusz Matusiewicz <mariuszmmm@op.pl>
2023-01-13 20:11:44 +0100

    changed function toggleTaskDone

--- src/useTasks.js
+++ src/useTasks.js
@@ -8,13 +8,9 @@ export const useTasks = () => {
    };
 
    const toggleTaskDone = (id) => {
-      setTasks(tasks => tasks.map(task => {
-         if (task.id === id) {
-            return { ...task, done: !task.done };
-         }
-
-         return task;
-      }));
+      setTasks(tasks => tasks.map(task =>
+         task.id === id ? { ...task, done: !task.done } : task
+      ));
    };
 
    const setAllDone = () => {
