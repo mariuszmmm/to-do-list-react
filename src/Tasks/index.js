cc4d3212b50b67bdccb962990df3eef43484df09
Mariusz Matusiewicz <mariuszmmm@op.pl>
2023-01-13 20:39:42 +0100

    changed spaces

--- src/Tasks/index.js
+++ src/Tasks/index.js
@@ -1,33 +1,33 @@
 import { List, Item, Button, Content, Text } from "./styled";
 
 const Tasks = ({ tasks, hideDone, removeTasks, toggleTaskDone }) => (
-	<List>
-		{tasks.map(task => (
-			<Item
-				key={task.id}
-				hidden={task.done && hideDone}
-			>
-				<Button
-					toggleDone
-					onClick={() => toggleTaskDone(task.id)}
-				>
-					{task.done ? "✔" : ""}
-				</Button>
-				<Content>
-					{task.id}.{" "}
-					<Text done={task.done}>
-						&nbsp;{task.content}&nbsp;
-					</Text>
-				</Content>
-				<Button
-					remove
-					onClick={() => removeTasks(task.id)}
-				>
-					🗑︎
-				</Button>
-			</Item>
-		))}
-	</List>
+   <List>
+      {tasks.map(task => (
+         <Item
+            key={task.id}
+            hidden={task.done && hideDone}
+         >
+            <Button
+               toggleDone
+               onClick={() => toggleTaskDone(task.id)}
+            >
+               {task.done ? "✔" : ""}
+            </Button>
+            <Content>
+               {task.id}.{" "}
+               <Text done={task.done}>
+                  &nbsp;{task.content}&nbsp;
+               </Text>
+            </Content>
+            <Button
+               remove
+               onClick={() => removeTasks(task.id)}
+            >
+               🗑︎
+            </Button>
+         </Item>
+      ))}
+   </List>
 );
 
 export default Tasks;
