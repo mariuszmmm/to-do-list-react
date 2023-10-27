194d0dce258e0c7f2cc5a50362c10cb4ea15ff9d
Mariusz Matusiewicz <mariuszmmm@op.pl>
2023-10-27 21:59:22 +0200

    changed main container

--- src/Container/index.js
+++ src/Container/index.js
@@ -1,9 +1,13 @@
-import { Main } from "./styled";
+import styled from "styled-components";
 
-const Container = ({ children }) => (
-   <Main>
-      {children}
-   </Main>
-);
+const Container = styled.main`
+   max-width: 900px;
+   padding: 20px;
+   margin: 0 auto;
+
+   @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
+      padding: 2%;
+   }
+`;
 
 export default Container;
