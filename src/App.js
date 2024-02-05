f8f031f729a43deeb64edb84acde7f8618609ae2
Mariusz Matusiewicz <mariuszmmm@op.pl>
2024-02-05 18:56:49 +0100

    the location of the Navigation component has been changed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { HashRouter, Routes, Route, Navigate } from "react-router-dom";
-import Navigation from "./common/Navigation";
+import Navigation from "./Navigation";
 import TaskPage from "./features/tasks/TaskPage";
 import TasksPage from "./features/tasks/TasksPage";
 import AuthorPage from "./features/AuthorPage";
@@ -9,6 +9,7 @@ const App = () => {
   return (
     <HashRouter>
       <Navigation />
+
       <Routes>
         <Route path="/zadania/:id" element={<TaskPage />} />
         <Route path="/zadania" element={<TasksPage />} />
