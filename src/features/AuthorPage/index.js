6015ce3de652e08f86a89a9b2c88afcef0d51f96
Mariusz Matusiewicz <mariuszmmm@op.pl>
2025-01-08 10:45:35 +0100

    smal corection

--- src/features/AuthorPage/index.js
+++ src/features/AuthorPage/index.js
@@ -13,7 +13,7 @@ const AuthorPage = () => {
           <>
             <Text>Tworzenie <i>frontendu</i> to moja pasja, zwłaszcza z&nbsp;wykorzystaniem <strong>React</strong>. Uwielbiam zgłębiać nowe technologie i&nbsp;stale rozwijać swoje umiejętności. Największą satysfakcję daje mi projektowanie intuicyjnych i&nbsp;estetycznych interfejsów, które ułatwiają życie użytkownikom.</Text><br />
             <Text>Poza programowaniem kocham góry. Wędrówki to dla mnie sposób na odpoczynek i&nbsp;naładowanie baterii. Szczególnie bliskie mojemu sercu są <strong>Bieszczady</strong> – ich spokój i&nbsp;naturalne piękno inspirują mnie za&nbsp;każdym razem, gdy tam wracam.
-              Łączę zamiłowanie do technologii z&nbsp;ciekawością świata. Dzięki temu z&nbsp;nientuzjazmem podejmuję nowe wyzwania, które pozwalają mi rozwijać się i&nbsp;tworzyć projekty, z których mogę być dumny. 😊🚀
+              Łączę zamiłowanie do technologii z&nbsp;ciekawością świata. Dzięki temu z&nbsp;entuzjazmem podejmuję nowe wyzwania, które pozwalają mi rozwijać się i&nbsp;tworzyć projekty, z których mogę być dumny. 😊🚀
             </Text>
           </>}
       />
