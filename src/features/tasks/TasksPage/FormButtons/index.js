a590d20b45dd0f2f1d978f0fad66e2fc0ead05a4
Mariusz Matusiewicz <mariuszmmm@op.pl>
2025-01-08 08:45:26 +0100

    Downloading sample tasks has been disabled if they already exist.

--- src/features/tasks/TasksPage/FormButtons/index.js
+++ src/features/tasks/TasksPage/FormButtons/index.js
@@ -1,17 +1,18 @@
 import { useDispatch, useSelector } from "react-redux";
-import { selectFetchStatus, fetchExampleTasks } from "../../tasksSlice";
+import { selectFetchStatus, fetchExampleTasks, selectTasks } from "../../tasksSlice";
 import ButtonsContainer from "../../../../common/ButtonsContainer";
 import Button from "../../../../common/Button";
 
 const FormButtons = () => {
   const dispatch = useDispatch();
   const fetchStatus = useSelector(selectFetchStatus);
+  const tasks = useSelector(selectTasks);
 
   return (
     <ButtonsContainer>
       <Button
         onClick={() => dispatch(fetchExampleTasks())}
-        disabled={fetchStatus === "loading" || fetchStatus === "error"}
+        disabled={fetchStatus === "loading" || fetchStatus === "error" || tasks.length > 0}
         error={fetchStatus === "error"}
       >
         {fetchStatus === "ready" ? "Pobierz przykładowe zadania" :
