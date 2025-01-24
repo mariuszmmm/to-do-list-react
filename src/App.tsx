230f4f37d4ac670e6a22a4e8d5dfee2ed549879b
Mariusz Matusiewicz <mariuszmmm@op.pl>
2025-01-24 07:07:38 +0100

    test

--- src/App.tsx
+++ src/App.tsx
@@ -7,22 +7,30 @@ import Container from "./common/Container";
 import CurrentDate from "./common/CurrentDate";
 import ListsPage from "./features/ListsPage";
 import Login from "./features/Login";
+import netlifyIdentity from "netlify-identity-widget";
 
-const App = () => (
-  <HashRouter>
-    <Navigation />
-    <Container>
-      <CurrentDate />
-      <Routes>
-        <Route path="/zadania/:id" element={<TaskPage />} />
-        <Route path="/zadania" element={<TasksPage />} />
-        <Route path="/listy" element={<ListsPage />} />
-        <Route path="/autor" element={<AuthorPage />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/" element={<Navigate to="/zadania" />} />
-      </Routes>
-    </Container>
-  </HashRouter>
-);
+netlifyIdentity.init();
+
+const App = () => {
+  const user = netlifyIdentity.currentUser();
+  console.log(user);
+
+  return (
+    <HashRouter>
+      <Navigation />
+      <Container>
+        <CurrentDate />
+        <Routes>
+          <Route path="/zadania/:id" element={<TaskPage />} />
+          <Route path="/zadania" element={<TasksPage />} />
+          <Route path="/listy" element={<ListsPage />} />
+          <Route path="/autor" element={<AuthorPage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/" element={<Navigate to="/zadania" />} />
+        </Routes>
+      </Container>
+    </HashRouter>
+  );
+};
 
 export default App;
