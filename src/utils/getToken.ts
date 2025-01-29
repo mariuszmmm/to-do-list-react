8fd216393ac8297fe683ea76f6b156ba69c84661
Mariusz Matusiewicz <mariuszmmm@op.pl>
2025-01-29 06:43:57 +0100

    test for register

--- src/utils/getToken.ts
+++ src/utils/getToken.ts
@@ -4,8 +4,8 @@ export const getToken = (url: string) => {
   if (url.includes("#confirmation_token")) {
     const token = url.split("#confirmation_token=")[1];
     saveTokenInLocalStorage(token);
-    // window.location.href = "https://to-do-list-typescript-react.netlify.app/#/konto";
-    window.location.href = "http://localhost:8888/#/konto";
-    saveTokenInLocalStorage(token);
+    window.location.href =
+      "https://to-do-list-typescript-react.netlify.app/#/konto";
+    // window.location.href = "http://localhost:8888/#/konto";
   }
 };
