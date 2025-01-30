8e31faca463f9949564e4f9fb423c26c611bac08
Mariusz Matusiewicz <mariuszmmm@op.pl>
2025-01-30 04:58:13 +0100

    test

--- src/features/Account/auth.tsx
+++ src/features/Account/auth.tsx
@@ -3,5 +3,5 @@ import GoTrue from "gotrue-js";
 export const auth = new GoTrue({
   APIUrl: "https://to-do-list-typescript-react.netlify.app/.netlify/identity",
   audience: "",
-  setCookie: true, // setCookie(optional): set to be false by default. If you wish to implement the remember me functionality, set the value to be true
+  setCookie: false, // setCookie(optional): set to be false by default. If you wish to implement the remember me functionality, set the value to be true
 });
