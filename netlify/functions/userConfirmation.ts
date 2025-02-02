50f348586f76c283c1d67b8b1fd101352b4e721c
Mariusz Matusiewicz <mariuszmmm@op.pl>
2025-02-02 06:55:24 +0100

    test

--- netlify/functions/userConfirmation.ts
+++ netlify/functions/userConfirmation.ts
@@ -4,7 +4,6 @@ let users: { email: string; confirmed: boolean }[] = [
   { email: "test@poczta.pl", confirmed: false },
 ];
 let test: any;
-let test1: any;
 
 const handler: Handler = async (event: HandlerEvent) => {
   if (event.httpMethod === "POST") {
@@ -12,8 +11,7 @@ const handler: Handler = async (event: HandlerEvent) => {
       const { email, confirmed } = JSON.parse(event.body);
       users.push({ email, confirmed });
     }
-    test = event.body;
-    test1 = event;
+    if (event.body) test = JSON.parse(event.body);
 
     return {
       statusCode: 200,
@@ -34,7 +32,6 @@ const handler: Handler = async (event: HandlerEvent) => {
         user,
         users,
         test,
-        test1,
       }),
     };
   }
