bc5feb44568551b9449e490642e38b5d25f01c26
Mariusz Matusiewicz <mariuszmmm@op.pl>
2025-02-05 06:54:00 +0100

    test

--- src/features/ConfirmationPage/index.tsx
+++ src/features/ConfirmationPage/index.tsx
@@ -1,9 +1,25 @@
+import { useEffect } from "react";
 import Header from "../../common/Header";
+import { useLocation, useNavigate } from "react-router-dom";
 
 export const ConfirmationPage = () => {
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  const searhParams = new URLSearchParams(location.search);
+  const confirmation = searhParams.get("confirmation");
+
+  useEffect(() => {
+    if (confirmation !== "success" && confirmation !== "error") {
+      navigate("/");
+    }
+  }, [confirmation, navigate]);
+
   return (
-    <>
-      <Header title="Rejestacja udana" />
-    </>
+    <Header
+      title={
+        confirmation === "success" ? "Rejestacja udana" : "Rejestacja nieudana"
+      }
+    />
   );
 };
