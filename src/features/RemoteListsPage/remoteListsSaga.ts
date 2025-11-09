ef750ece5b7232fc9e69789940fd77feb0c266c8
Mariusz Matusiewicz <mariuszmmm@op.pl>
2025-11-09 12:06:33 +0100

    Update remoteListsSaga.ts

--- src/features/RemoteListsPage/remoteListsSaga.ts
+++ src/features/RemoteListsPage/remoteListsSaga.ts
@@ -1,4 +1,4 @@
-import { call, put, delay, select, takeLatest } from "redux-saga/effects";
+import { put, delay, select, takeLatest } from "redux-saga/effects";
 import { selectToUpdate, setToUpdate } from "../tasks/tasksSlice";
 
 export function* pollDataSaga() {
