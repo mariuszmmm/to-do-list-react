c04beb85870527fbe353b5724185c3f885f3934c
Mariusz Matusiewicz <mariuszmmm@op.pl>
2025-12-07 17:28:20 +0100

    Update index.tsx

--- src/features/tasks/TaskPage/index.tsx
+++ src/features/tasks/TaskPage/index.tsx
@@ -37,10 +37,10 @@ const TaskPage = () => {
                 <Name>{t("dateCreated")}:</Name>
                 {formatCurrentDate(new Date(task.date), i18n.language)}
               </DateInfo>
-              {task.editedAt && (
+              {task.updatedAt && (task.updatedAt !== task.date) && (
                 <DateInfo>
-                  <Name>{t("dateEdited")}:</Name>
-                  {formatCurrentDate(new Date(task.editedAt), i18n.language)}
+<Name>{t("dateEdited")}:</Name>
+                  {formatCurrentDate(new Date(task.updatedAt), i18n.language)}
                 </DateInfo>
               )}
               {task.done && task.completedAt && (
